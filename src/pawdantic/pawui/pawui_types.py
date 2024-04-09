cb0dd2726763938760fc2129f2af5c6051ccb672
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2024-04-09 22:53:06 +0100

    run away

--- src/pawdantic/pawui/pawui_types.py
+++ src/pawdantic/pawui/pawui_types.py
@@ -3,9 +3,6 @@ from __future__ import annotations
 import typing as _t
 
 import pydantic as _p
-from fastui import components as c
 
-ModalType = tuple[c.Button, c.Modal]
 AlertType = _t.Literal['ERROR', 'WARNING', 'NOTIFICATION', 'INFO']
-
 AlertDict = _t.Annotated[dict[str, AlertType], _p.Field(default_factory=dict)]
