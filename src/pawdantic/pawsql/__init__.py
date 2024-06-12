4001507101f2eae1824e059f8ecfd5d6da18f2d0
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2024-06-12 01:55:06 +0100

    move generic jsoin to init

---
+++ src/pawdantic/pawsql/__init__.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+from pydantic import BaseModel
+
+
+class GenericJSONType(sqlalchemy.TypeDecorator):
+    impl = sqlalchemy.JSON
+
+    def __init__(self, model_class: type[BaseModel], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model_class = model_class
+
+    def process_bind_param(self, value, dialect):
+        return value.model_dump_json(round_trip=True) if value is not None else None
+
+    def process_result_value(self, value, dialect):
+        return self.model_class.model_validate_json(value) if value else None
--- src/pawdantic/pawsql/sqlpr.py
+++ src/pawdantic/pawsql/sqlpr.py
@@ -80,16 +80,3 @@ def get_other_table_names(obj, data_models) -> list[str]:
     """
     return [related_from_snakenames(obj, _) for _ in data_models if not isinstance(obj, _)]
 
-
-class GenericJSONType(sqa.TypeDecorator):
-    impl = sqa.JSON
-
-    def __init__(self, model_class: type[BaseModel], *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.model_class = model_class
-
-    def process_bind_param(self, value, dialect):
-        return value.model_dump_json(round_trip=True) if value is not None else None
-
-    def process_result_value(self, value, dialect):
-        return self.model_class.model_validate_json(value) if value else None
