a34e8faf6b7ba4949d4d410e48737ba62a2e1092
paw <pawrequest@users.noreply.github.com>
2024-10-25 14:07:17 +0100

    rm get_hash

--- src/pawdantic/fastpaw/__init__.py
+++ src/pawdantic/fastpaw/__init__.py
@@ -1,5 +1,6 @@
 import inspect
 from typing import Annotated
+from pydantic import BaseModel
 
 from fastapi import Form
 
@@ -19,6 +20,29 @@ def as_form(cls):
 
     return cls
 
+def as_form_old(cls: type[BaseModel]):
+    new_parameters = []
+
+    for field_name, model_field in cls.model_fields.items():
+        model_field: ModelField  # type: ignore
+
+        new_parameters.append(
+             inspect.Parameter(
+                 model_field.alias,
+                 inspect.Parameter.POSITIONAL_ONLY,
+                 default=Form(...) if model_field.required else Form(model_field.default),
+                 annotation=model_field.outer_type_,
+             )
+         )
+
+    async def as_form_func(**data):
+        return cls(**data)
+
+    sig = inspect.signature(as_form_func)
+    sig = sig.replace(parameters=new_parameters)
+    as_form_func.__signature__ = sig  # type: ignore
+    setattr(cls, 'as_form', as_form_func)
+    return cls
 
 """
 https://stackoverflow.com/questions/60127234/how-to-use-a-pydantic-model-with-form-data-in-fastapi/77113651#77113651
